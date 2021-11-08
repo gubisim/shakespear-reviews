fcf82fc2f235d33ccd0453da17a21e99797440fa
Brad Terry <brad@ec2software.com>
2021-11-07 21:18:22 -0800

    Brand link and update sub-headline text

--- src/js/Home.jsx
+++ src/js/Home.jsx
@@ -1,20 +1,24 @@
 import "../css/Home.css";
+import { Link } from "react-router-dom";
 
 export default function Home() {
   return (
     <div>
       <div className="main-container flex">
         <div className="content-container">
-          <div className="brand">
-            <a>Shakespeare Reviews</a>
-          </div>
+          <Link to="/">
+            <div className="brand">Shakespeare Reviews</div>
+          </Link>
           <h1 className="headline">
             100+ reviews of Shakespeare's beloved classics
           </h1>
           <p className="content sub-headline">
-            See what the thinkers of today are saying and how they are interpreting the works of William Shakespeare.
+            See what the thinkers of today are saying and how they are
+            interpreting the works of William Shakespeare.
           </p>
-          <button className="primary-button">Read Reviews</button>
+          <Link to="/reviews">
+            <button className="primary-button">Read Reviews</button>
+          </Link>
         </div>
         <div className="image-container"></div>
       </div>
