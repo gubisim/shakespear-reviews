879b94340d91a6c8b8d860e3ff29d93af15f22e5
Brad Terry <brad@ec2software.com>
2021-11-07 21:17:54 -0800

    Brand Link component

---
+++ src/js/Brand.jsx
@@ -0,0 +1,9 @@
+import { Link } from "react-router-dom";
+
+export default function Brand() {
+  return (
+    <Link to="/">
+      <div className="brand">Shakespeare Reviews</div>
+    </Link>
+  );
+}
