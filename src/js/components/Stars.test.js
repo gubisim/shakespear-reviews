2a244a9c8cf22bb2db9838ac58637d8c5b7e5c7d
Brad Terry <bradleydeanter@gmail.com>
2021-11-10 10:09:14 -0800

    add tests on Stars component rendering images correctly

--- src/js/components/Stars.test.js
+++ src/js/components/Stars.test.js
@@ -1,8 +1,34 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import Stars from './Stars';
+import { render, screen } from '@testing-library/react';
 
 it('renders without crashing', () => {
   const div = document.createElement('div');
   ReactDOM.render(<Stars rating={3.5} />, div);
+});
+
+it('renders 3 full stars', () => {
+    render(<Stars rating={3} />);
+    expect(screen.getAllByAltText('Filled in star').length).toBe(3)
+});
+
+it('renders 5 full stars on number larger than 5', () => {
+    render(<Stars rating={5} />);
+    expect(screen.getAllByAltText('Filled in star').length).toBe(5)
+});
+
+it('renders 5 empty stars on no rating prop passed', () => {
+    render(<Stars/>);
+    expect(screen.getAllByAltText('Empty star').length).toBe(5)
+});
+
+it('renders 5 empty stars on negative rating passed', () => {
+    render(<Stars rating={-1}/>);
+    expect(screen.getAllByAltText('Empty star').length).toBe(5)
+});
+
+it('renders half star on rating with decimal', () => {
+    render(<Stars rating={3.5}/>);
+    expect(screen.getAllByAltText('Half filled in star').length).toBe(1)
 });
